8fd047b654fd99fe9523fc7fcbf454d533f1df85
root <cooperg122@gmail.com>
2025-02-02 21:49:06 -0700

    refactor visuals

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import pandas as pd
 from data_processing import get_asset_data
 from optimization import optimize_portfolio
 from portfolio_stats import portfolio_return_and_risk
-from visualizations import get_efficient_frontier, get_random_portfolios, get_sharpe_ratios_and_returns, plot_random_portfolios_with_EF, plot_sharpe_ratio_vs_returns 
+from visualizations import get_efficient_frontier_data, get_random_portfolios, plot_random_portfolios_with_EF, plot_scatter 
 
 import random
 
@@ -15,6 +15,7 @@ RISK_FREE_RETURN = 0.03
 TARGET_RETURN = 0.15
 
 # Efficient frontier and Sharpe ratio settings
+# From 0.05 to 0.8 jumping by 0.015
 MIN_RETURN = 0.05
 MAX_RETURN = 0.8
 NUM_RETURNS = 51
@@ -28,7 +29,7 @@ def main():
     # SPLK, FISV, SGEN
     tickers_df = pd.read_csv("../nasdaq100.csv")
     tickers = tickers_df["Ticker"].tolist()
-    tickers = random.sample(tickers, 10)
+    tickers = random.sample(tickers, 20)
 
     # Get expected returns vector and covariance matrix
     returns, cov_matrix = get_asset_data(tickers, TOTAL_DAYS_BACK)
@@ -38,14 +39,12 @@ def main():
     portfolio_return, portfolio_risk = portfolio_return_and_risk(returns, cov_matrix, weights)
 
     # Get the efficient fronteir data points
-    ef_risks, ef_returns = get_efficient_frontier(returns, cov_matrix, MIN_RETURN, MAX_RETURN, NUM_RETURNS)
-    random_risks, random_returns = get_random_portfolios(returns, cov_matrix, RANDOM_PORTFOLIOS)
+    ef_returns, ef_risks, ef_sharp_ratios = get_efficient_frontier_data(returns, cov_matrix, RISK_FREE_RETURN, MIN_RETURN, MAX_RETURN, NUM_RETURNS)
+    random_returns, random_risks = get_random_portfolios(returns, cov_matrix, RANDOM_PORTFOLIOS)
 
     plot_random_portfolios_with_EF(ef_risks, ef_returns, random_risks, random_returns)
-
-    # Plot Sharpe ratio vs returns
-    portfolio_returns, sharpe_ratios = get_sharpe_ratios_and_returns(returns, cov_matrix, RISK_FREE_RETURN, MIN_RETURN, MAX_RETURN, NUM_RETURNS)
-    plot_sharpe_ratio_vs_returns(portfolio_returns, sharpe_ratios)
+    plot_scatter(ef_returns, ef_sharp_ratios, xlabel="Return", ylabel="Sharpe Ratio")
+    plot_scatter(ef_risks, ef_sharp_ratios, xlabel="Risk", ylabel="Sharpe Ratio")
 
 if __name__ == "__main__":
     main()
--- src/visualizations.py
+++ src/visualizations.py
@@ -4,10 +4,10 @@ import matplotlib.pyplot as plt
 from optimization import optimize_portfolio
 from portfolio_stats import portfolio_return_and_risk, portfolio_sharpe_ratio
 
-def get_efficient_frontier(returns, cov_matrix, min_return, max_return, num_returns):
+def get_efficient_frontier_data(returns, cov_matrix, risk_free_return, min_return, max_return, num_returns):
     """
-    Calculates a series of returns and their correesponding minimum risks, which can be used 
-    to plot the efficient frontier.
+    Calculates a series of returns and their risks and Sharpe ratios. Used to plot the efficient frontier
+    and Sharpe ratio visuals.
 
     Args:
         returns (np.array): Expected returns for each asset (1D array).
@@ -17,31 +17,37 @@ def get_efficient_frontier(returns, cov_matrix, min_return, max_return, num_retu
         num_returns (float): Amount of portfolios calculated.
     
     Returns:
-        tuple: (risks, actual_returns)
-            - risks (list): The risk of each portfolio.
+        tuple: (actual_returns, risks, shrpe_ratios)
             - actual_return (list): The actual return of each portfolio, which can sometimes be
             higher than the target return.
+            - risks (list): The risk of each portfolio.
+            - sharpe_ratios (list): The Sharpe ratios of each portfolio.
+
     """
     target_returns = np.linspace(min_return, max_return, num_returns)
 
-    risks = []
     actual_returns = []
+    risks = []
+    sharpe_ratios = []
     for target_return in target_returns:
         try:
             weights = optimize_portfolio(returns, cov_matrix, target_return)
             portfolio_return, portfolio_risk = portfolio_return_and_risk(returns, cov_matrix, weights)
+            sharpe_ratio = portfolio_sharpe_ratio(portfolio_return, portfolio_risk, risk_free_return)
         except (ValueError, cp.error.SolverError) as e:
             # This will occur if there is not a feasible solution with the given
-            # portfolio and target return
+            # assets and target return
             print(f"Infeasible solution for target return: {target_return}, Error: {str(e)}")
-            risks.append(np.nan)
             actual_returns.append(np.nan)
+            risks.append(np.nan)
+            sharpe_ratios.append(np.nan)
             continue 
 
-        risks.append(portfolio_risk)
         actual_returns.append(portfolio_return)
+        risks.append(portfolio_risk)
+        sharpe_ratios.append(sharpe_ratio)
 
-    return risks, actual_returns
+    return actual_returns, risks, sharpe_ratios
      
 def get_random_portfolios(returns, cov_matrix, amount):
     """
@@ -53,10 +59,10 @@ def get_random_portfolios(returns, cov_matrix, amount):
         amount (int): Amount of portfolios generated.
 
     Returns:
-        tuple: (risks, actual_returns)
-            - risks (list): The risk of each portfolio.
+        tuple: (actual_returns, risks)
             - actual_return (list): The return of each portfolio. Theres no optimization
             here so there is no target return.
+            - risks (list): The risk of each portfolio.
     """
     num_assets = len(returns)
     risks = []
@@ -70,60 +76,27 @@ def get_random_portfolios(returns, cov_matrix, amount):
         risks.append(portfolio_risk)
         actual_returns.append(portfolio_return)
 
-    return risks, actual_returns
+    return actual_returns, risks 
 
-def get_sharpe_ratios_and_returns(returns, cov_matrix, risk_free_return, min_return, max_return, num_returns):
+def plot_scatter(x, y, line=False, xlabel="X-axis", ylabel="Y-axis", title="Scatter Plot", color="blue"):
     """
-    Calculates a series of returns and their correesponding Sharpe ratios.
-
+    General scatter plot function for return-risk or Sharpe ratio-return plots.
+    
     Args:
-        returns (np.array): Expected returns for each asset (1D array).
-        cov_matrix (np.array): Covariance matrix of asset returns (2D array).
-        min_return (float): Smallest target return.
-        max_return (float): Largest target return.
-        num_returns (float): Amount of portfolios calculated.
-    
-    Returns:
-        tuple: (actual_returns, sharpe_ratios)
-            - actual_returns (list): The actual return of each portfolio, which can sometimes be
-            higher than the target return.
-            - sharpe_ratios (list): The Sharpe ratios of each portfolio.
+    - x: Data for the x-axis.
+    - y: Data for the y-axis.
+    - line: (bool) Setting to True will plot a line connecting all points.
+    - xlabel: Label for the x-axis.
+    - ylabel: Label for the y-axis.
+    - title: Title of the graph.
+    - color: Color of the scatter points.
     """
-    target_returns = np.linspace(min_return, max_return, num_returns)
-
-    actual_returns = []
-    sharpe_ratios = []
-    for target_return in target_returns:
-        try:
-            weights = optimize_portfolio(returns, cov_matrix, target_return)
-            portfolio_return, portfolio_risk = portfolio_return_and_risk(returns, cov_matrix, weights)
-            sharpe_ratio = portfolio_sharpe_ratio(portfolio_return, portfolio_risk, risk_free_return)
-        except (ValueError, cp.error.SolverError) as e:
-            # This will occur if there is not a feasible solution with the given
-            # portfolio and target return
-            print(f"Infeasible solution for target return: {target_return}, Error: {str(e)}")
-            actual_returns.append(np.nan)
-            sharpe_ratios.append(np.nan)
-            continue 
-
-        actual_returns.append(portfolio_return)
-        sharpe_ratios.append(sharpe_ratio)
-    
-    return actual_returns, sharpe_ratios
-
-def plot_efficient_frontier(risks, actual_returns):
-    """
-    Plots the efficient frontier line and points using risk and return values.
-    """
-    plt.plot(risks, actual_returns)
-    plt.scatter(risks, actual_returns)
-    plt.show()
-
-def plot_random_portfolios(risks, actual_returns):
-    """
-    Plots portfolios as points on the return vs. risk graph, intended for the randomly weighted portfolios.
-    """
-    plt.scatter(risks, actual_returns)
+    plt.scatter(x, y, c=color)
+    if line:
+        plt.plot(x, y)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
     plt.show()
 
 
@@ -134,11 +107,4 @@ def plot_random_portfolios_with_EF(ef_risks, ef_returns, random_risks, random_re
     plt.scatter(random_risks, random_returns, c="blue")
     plt.plot(ef_risks, ef_returns, "orange")
     plt.scatter(ef_risks, ef_returns, c="orange")
-    plt.show()
-
-def plot_sharpe_ratio_vs_returns(actual_returns, sharpe_ratios):
-    """
-    Plots portfolios as points on the Sharpe ratio vs return graph.
-    """
-    plt.scatter(actual_returns, sharpe_ratios)
     plt.show()
